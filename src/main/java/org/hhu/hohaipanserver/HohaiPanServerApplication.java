70c3ba980a37596abf6b1578507c88091c4c5812
SakuraChiyo0v0 <3296299414@qq.com>
2024-12-02 01:20:06 +0800

    1

---
+++ src/main/java/org/hhu/hohaipanserver/HohaiPanServerApplication.java
@@ -0,0 +1,13 @@
+package org.hhu.hohaipanserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HohaiPanServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HohaiPanServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hhu/hohaipanserver/HohaiPanServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hhu.hohaipanserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HohaiPanServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
